ad5672bcddf8324a8c74bf836693aaa1269d2f59
Andrey Malin <andreu6454@gmail.com>
2022-12-15 21:56:14 +0300

    -добавил provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { grey } from '@mui/material/colors';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import { routesConfig } from './routes/routes-config';
+import { setupStore } from './store/store';
 
 const themeLight = createTheme({
   palette: {
@@ -15,15 +17,18 @@ const themeLight = createTheme({
     },
   },
 });
+const store = setupStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={createTheme(themeLight)}>
-      <CssBaseline />
-      <RouterProvider router={routesConfig} />
-    </ThemeProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <ThemeProvider theme={createTheme(themeLight)}>
+        <CssBaseline />
+        <RouterProvider router={routesConfig} />
+      </ThemeProvider>
+    </React.StrictMode>
+  </Provider>,
 );
 
 reportWebVitals();
