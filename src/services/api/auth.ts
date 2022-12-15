4ab1aec731460d29197d4a29d3876c522524b0fe
evgenij <onethps@gmail.com>
2022-12-16 01:53:28 +0200

    -added portal form alert error, -added auth slice, fixed ui issues, added error util, added auth thunk

--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -1,3 +1,27 @@
+import { AxiosResponse } from 'axios';
+
+import { instance } from './../config';
+
+export interface GetLoginType {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+}
+
+export interface ResponseLoginDataType {
+  _id: string;
+  email: string;
+  name: string;
+  avatar?: string;
+  publicCardPacksCount: number;
+  created: Date;
+  updated: Date;
+  isAdmin: boolean;
+  verified: boolean;
+  rememberMe: boolean;
+  error?: string;
+}
+
 export const authAPI = {
   me() {
     //TODO:me point
@@ -7,5 +31,12 @@ export const authAPI = {
   },
   newPassword() {
     //TODO: newPassword point
+  },
+  login({ email, password, rememberMe }: GetLoginType) {
+    return instance.post<ResponseLoginDataType>('/auth/login', {
+      email,
+      password,
+      rememberMe,
+    });
   },
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,18 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 
 import { appReducer } from '../App/app-slice';
+import { authSlice } from '../features/auth/authSlice';
 
 const rootReducer = combineReducers({
   app: appReducer,
+  auth: authSlice,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
   });
 };
 
