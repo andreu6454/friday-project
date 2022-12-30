b6d146e663939ed9e012e4259f727c6d6487088d
onethps <onethps@gmail.com>
2022-12-30 05:27:35 +0200

    🐞 fix(learn page):
    fix back button

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './error-utils';
 export * from './portal-wrapper';
+export * from './random-card';
---
+++ src/utils/random-card.ts
@@ -0,0 +1,15 @@
+import { ICard } from './../services/api/cards';
+
+export const getCard = (cards: ICard[]) => {
+  const sum = cards.reduce((acc, card) => acc + (6 - card.grade) * (6 - card.grade), 0);
+  const rand = Math.random() * sum;
+  const res = cards.reduce(
+    (acc: { sum: number; id: number }, card, i) => {
+      const newSum: number = acc.sum + (6 - card.grade) * (6 - card.grade);
+      return { sum: newSum, id: newSum < rand ? i : acc.id };
+    },
+    { sum: 0, id: -1 },
+  );
+
+  return cards[res.id + 1];
+};
